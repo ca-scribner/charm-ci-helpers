4316143ab3b1f741a3296faf33378c474c60d316
Andrew Scribner <ca.scribner+1@gmail.com>
2022-07-14 16:34:13 -0400

    add revision updates, file io, and differencing to Bundle

--- bundle.py
+++ bundle.py
@@ -1,24 +1,85 @@
+import copy
 from pathlib import Path
+from typing import Optional
+
 import yaml
+
+from deepdiff import DeepDiff
+from juju import Juju
 
 
 class Bundle:
-    def __init__(self, filename: str):
+    def __init__(self, filename: Optional[str] = None):
         self._filename = filename
         self._data = None
-        self.load_bundle()
+        if self._filename:
+            self.load_bundle()
 
-    def load_bundle(self):
-        self._data = yaml.safe_load(Path(self._filename).read_text())
+    def deepcopy(self):
+        """Returns a new deep copy of this Bundle"""
+        newbundle = copy.deepcopy(self)
+        return newbundle
 
-    @property
-    def applications(self):
-        return self._data['applications']
-
-    def to_dict(self):
-        return self._data
+    def diff(self, other):
+        """Returns a diff between this and another object, in DeepDiff format"""
+        return DeepDiff(self._data, other._data, ignore_order=True)
 
     def dump(self, filename: str):
         """Dumps as yaml to a file"""
         with open(filename, 'w') as fout:
             yaml.dump(self.to_dict(), fout)
+
+    def get_latest_revisions(self):
+        """Return a new Bundle that has the latest revisions of all charms in this bundle"""
+        newbundle = self.deepcopy()
+
+        applications = newbundle.applications
+
+        for name, application in applications.items():
+            # Skip charms we're not "tracking" a channel for
+            if "_tracked_channel" not in application:
+                continue
+
+            charm_name = application["charm"]
+            channel = application["_tracked_channel"]
+            revision = application.get("revision", None)
+
+            newest_revision = get_newest_charm_revision(charm_name, channel)
+
+            if revision != newest_revision:
+                # If revision is not up to date - update in situ from tracked channel
+                application["revision"] = newest_revision
+
+        return newbundle
+
+    def load_bundle(self):
+        """Loads a YAML file as a bundle"""
+        self._data = yaml.safe_load(Path(self._filename).read_text())
+
+    def to_dict(self):
+        return self._data
+
+    def __eq__(self, other):
+        return (
+            self._filename == other._filename
+            and self.diff(other) == {}
+        )
+
+    @property
+    def applications(self):
+        return self._data['applications']
+
+
+
+
+
+
+def get_newest_charm_revision(charm: str, channel: str):
+    """Returns the newest revision of a charm in a channel"""
+    charm_info = Juju.info(charm)
+    channel_map = charm_info["channel-map"]
+    try:
+        tracked_channel_release = channel_map[channel]
+    except KeyError:
+        raise ValueError(f"No channel {channel} in `juju info {charm} --format yaml`")
+    return tracked_channel_release["revision"]
--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-from juju import Juju
 from bundle import Bundle
-
-
-def get_newest_charm_revision(charm: str, channel: str):
-    """Returns the newest revision of a charm in a channel"""
-    charm_info = Juju.info(charm)
-    channel_map = charm_info["channel-map"]
-    try:
-        tracked_channel_release = channel_map[channel]
-    except KeyError:
-        raise ValueError(f"No channel {channel} in `juju info {charm} --format yaml`")
-    return tracked_channel_release["revision"]
 
 
 def main(source_bundle_file: str, output_bundle_file: str):
     bundle = Bundle(source_bundle_file)
-    applications = bundle.applications
+    updated_bundle = bundle.get_latest_revisions()
+    print(bundle.diff(updated_bundle))
 
-    for name, application in applications.items():
-        # Skip charms we're not "tracking" a channel for
-        if "_tracked_channel" not in application:
-            continue
-
-        charm_name = application["charm"]
-        channel = application["_tracked_channel"]
-        revision = application.get("revision", None)
-
-        newest_revision = get_newest_charm_revision(charm_name, channel)
-
-        if revision != newest_revision:
-            # If revision is not up to date - update in situ from tracked channel
-            application["revision"] = newest_revision
-
-    bundle.dump(output_bundle_file)
+    updated_bundle.dump(output_bundle_file)
 
 
 if __name__ == "__main__":
